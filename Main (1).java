42c90ee4268d34498babb8150613335bfd9efe21
Thiago souza <112909720+thsouzag@users.noreply.github.com>
2022-10-01 10:43:54 -0300

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+  Scanner console = new Scanner(System.in);
+  // Variavel
+    int N1;
+  // processamento
+    System.out.println("digite um valor ?");
+    N1 = console .nextInt();
+  // saida 
+    System.out.println("\n o numero  informado foi !\t" + N1);
+   
+  }
+}
